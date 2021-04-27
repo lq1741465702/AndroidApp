92283b49535c1a56a7c1fe78b2851c5d4afba243
lq1741465702 <queldorei1996@gmail.com>
2021-04-27 23:17:30 +0800

    2021.04.27 刘强根据Android App开发入门与项目实战提交Unit3代码

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,30 +4,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.myapplication.Util.*;
+
+import com.example.myapplication.Util.FunctionUtil;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    public Context mCotext;
+    public static Context mCotext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCotext=getApplicationContext();
-        FunctionUtil.getAppName(mCotext);
         TextView main_hello = findViewById(R.id.main_hello);
         TextView main_hello_dp = findViewById(R.id.mian_hello_dp);
         TextView main_hello_sp = findViewById(R.id.mian_hello_sp);
         TextView main_hello_px = findViewById(R.id.mian_hello_px);
-        LinearLayout mainLinearLayout = findViewById(R.id.mainLinearLayout);
-//        mainLinearLayout.setBackgroundColor(getResources().getColor(R.color.purple_700));
-//        main_hello.setText("3秒后跳入Ac3");
 
+
+
+        LinearLayout mainLinearLayout = findViewById(R.id.mainLinearLayout);
         main_hello_px.setTextColor(getResources().getColor(R.color.purple_200));
         main_hello_sp.setTextColor(getResources().getColor(R.color.black));
         main_hello_dp.setTextColor(getResources().getColor(R.color.green));
@@ -38,6 +41,32 @@ public class MainActivity extends AppCompatActivity {
         //修改布局参数中的宽度数值，注意默认px单位，需要把dp数值转为px数值
         params.width = FunctionUtil.dip2px(this,300);
         main_hello_dp.setLayoutParams(params);
+
+        main_hello_dp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondPage = new Intent(MainActivity.this, SecondActivity.class);
+                MainActivity.this.startActivity(secondPage);
+            }
+        });
+        main_hello_sp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secondPage = new Intent(MainActivity.this, Unit3Activity.class);
+                MainActivity.this.startActivity(secondPage);
+            }
+        });
+
+
+        Button btn_go_btn = (Button)findViewById(R.id.go_button);
+        btn_go_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent buttonActivity = new Intent(MainActivity.this,ButtonActivity.class);
+                startActivity(buttonActivity);
+            }
+        });
+
 
     }
 
@@ -54,12 +83,14 @@ public class MainActivity extends AppCompatActivity {
     private Runnable mGoMain3 = new Runnable() {
         @Override
         public void run() {
-            startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
         }
     };
 
 
-
+    public static Context getContext(){
+        return mCotext;
+    }
 
 
 }
--- app/src/main/java/com/example/myapplication/Util/FunctionUtil.java
+++ app/src/main/java/com/example/myapplication/Util/FunctionUtil.java
@@ -3,31 +3,35 @@ package com.example.myapplication.Util;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.util.Log;
+
+import com.example.myapplication.MainActivity;
 
 public class FunctionUtil {
-    public static String AppName ="" ;
+    public static String AppName ="";
     private static final String TAG = "FunctionUtil";
 
     //根据手机分辨率从dp的单位转为px(像素)
     public static int dip2px (Context context , float dpValue){
         //获取手机当前的像素密度(1个dp对应几个px)
         float scale = context.getResources().getDisplayMetrics().density;
-        Log.d(TAG, "scale =  "+scale);
+        LogUtil.d(TAG, "scale =  "+scale);
         return (int)(dpValue * scale + 0.5f);//四舍五入取整
     }
 
 
-    public static String getAppName(Context mCotext){
+    public static String getAppName(){
         try {
-            PackageManager mPackageManager = mCotext.getPackageManager();
-            PackageInfo mPackageInfo = mPackageManager.getPackageInfo(mCotext.getPackageName(),0);
+            if (("").equals(AppName)){
+            Context mContext =  MainActivity.getContext();
+            PackageManager mPackageManager =mContext.getPackageManager();
+            PackageInfo mPackageInfo = mPackageManager.getPackageInfo(mContext.getPackageName(),0);
             int labelRes = mPackageInfo.applicationInfo.labelRes;
-            AppName = mCotext.getResources().getString(labelRes);
+            AppName = mContext.getResources().getString(labelRes);
+            }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        Log.d(TAG, "getAppName: "+AppName);
         return AppName;
     }
+
 }
